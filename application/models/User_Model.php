4875185c458c74f09a23b1aa8a57c731e75efe54
Muda Wali Bin Abdullah <mudawali@cloone.com.my>
2021-06-29 14:15:55 +0800

    Create User_Model.php

---
+++ application/models/User_Model.php
@@ -0,0 +1,21 @@
+<?php
+class User_Model extends CI_Model 
+{
+	public $table = 'user';
+	public $primary_key = 'id';
+
+	public function read_login($username, $password, $type)
+	{
+		$db = $this->db;
+		$db->select('id, type, username, first_name, last_name, email, created_at, updated_at');
+		$db->where('deleted_at IS NULL', NULL, FALSE);
+		$db->where('username', $username);
+		$db->where('password', encryptor('encrypt', $password));
+		$db->where_in('type', $type);
+		$db->limit(1);
+		$rst = $db->get($this->table)->row();
+
+		return $rst;
+	}
+}
+?>
